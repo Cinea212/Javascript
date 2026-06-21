5113b955223efd5d85bdeabd1aa60e5a5d41ee43
Cinea212 <oommnerkar@gmail.com>
2026-06-21 11:02:23 +0000

    new file:   1_Basics/2_datatypes.js

---
+++ 1_Basics/1_variables.js
@@ -0,0 +1,16 @@
+const account_Id = 165785
+let account_Email = "oommnrkar@gmail.com"
+var account_Password = "12345"
+account_City = "Mumbai"
+let account_State;
+
+
+/* Another way to comment similar to C++ 
+Prefer not to use var to define variable due to issue in functional scope and block scope defining */
+//account_Id = 2
+account_Email = "hhvcs@ghj.com"
+account_Password = "67537"
+account_City = "Jaiselmer"
+console.log(account_Id);
+console.table([account_Id, account_Email, account_Password, account_City])
+console.log(account_State)
---
+++ 1_Basics/2_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) we are using nodejs not browser
+
+//datatypes
+let name = "Om"
+let age = 19
+let isLoggedIn = true
+let state = null
+
+// number
+// bigint for very big int
+// string " "
+// boolean true or false 
+// null it is a standalone value 
+// undefined for eg (let name; )
+// symbol = unique 
+
+// object 
+
+console.log(typeof "Om")
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof age)
+
+
+
