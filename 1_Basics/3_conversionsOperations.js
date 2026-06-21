26be04d1dcc691594fc211e100df7130a317a2d6
Cinea212 <oommnerkar@gmail.com>
2026-06-21 11:28:15 +0000

    new file:   1_Basics/3_conversionsOperations.js

---
+++ 1_Basics/3_conversionsOperations.js
@@ -0,0 +1,29 @@
+let score = "50"
+let name = "Om"
+let score1 = null
+let bool = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof (valueInNumber));
+console.log(typeof (score1));
+
+let valInNum = Number(score1)
+
+console.log(typeof (valInNum));
+console.log(score1)
+console.log(bool)
+console.log(typeof (bool));
+let val = Number(name)
+console.log(typeof (val));
+console.log(val)
+
+//let isLoggedIn = 1
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
